7c64803830814d3e91c6ed90dde7f34575f22d46
makeitprabha <prabakarcs40@gmail.com>
2021-12-19 20:22:44 +0530

    remove

--- bindings/bindings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:boa_task_app/controllers/bottom_navigation_bar_controller.dart'; 
-import 'package:get/get.dart';
-
-class GetBindings extends Bindings {
-  @override
-  void dependencies() {
-  Get.put<BottomNavigationBarController>(BottomNavigationBarController());  
-  }
-}
